cc31cce161f748e9830e3f115ff42ad63a11cef8
69SafonovaYV <69SafonovaYV@cbr.ru>
2020-01-09 15:30:22 +0700

    add command for start path initialize

--- src/com/home/Main.java
+++ src/com/home/Main.java
@@ -7,6 +7,13 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
+        
+        if (args.length == 0) {
+            System.out.println("путь не задан, но его можно задать при помощи команды -path c:\\mypath в консоли приложения");
+        } else {
+            SearchContext.init().add("path", args[0]);
+            System.out.println("путь к директории - " + args[0]+ " но его можно поменять при помощи команды -path c:\\mypath в консоли приложения");
+        }
 
         Scanner scan = new Scanner(System.in);
         String input;
